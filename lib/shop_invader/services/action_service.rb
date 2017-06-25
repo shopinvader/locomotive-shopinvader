41df47c791ae3b2033dc98a6e38c2e675a868499
Sébastien BEAU <sebastien.beau@akretion.com>
2017-06-25 16:44:40 +0200

    [IMP] add the storeCall action

---
+++ lib/shop_invader/services/action_service.rb
@@ -0,0 +1,26 @@
+require 'locomotive/steam/services'
+
+module Locomotive
+  module Steam
+
+    class ActionService
+      alias_method :orig_define_built_in_functions, :define_built_in_functions
+
+      SHOPINVADER_BUILT_IN_FUNCTIONS = %w(storeCall)
+
+      def define_built_in_functions(context, liquid_context)
+        orig_define_built_in_functions(context, liquid_context)
+        puts '2'
+        SHOPINVADER_BUILT_IN_FUNCTIONS.each do |name|
+          context.define_function name, &send(:"#{name.underscore}_lambda", liquid_context)
+        end
+        puts '3'
+      end
+
+      def store_call_lambda(liquid_context)
+        erp = liquid_context.registers[:services].erp
+        -> (method, path, params) { erp.call(method, path, params) }
+      end
+    end
+  end
+end
