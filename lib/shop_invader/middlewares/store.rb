8a1bb2730e8b73aee4685dce8f24262b8d197797
Sébastien BEAU <sebastien.beau@akretion.com>
2018-10-01 17:47:19 +0200

    [FIX] fix include due to change in steam

--- lib/shop_invader/middlewares/store.rb
+++ lib/shop_invader/middlewares/store.rb
@@ -2,7 +2,7 @@ module ShopInvader
   module Middlewares
     class Store < Locomotive::Steam::Middlewares::ThreadSafe
 
-      include Locomotive::Steam::Middlewares::Helpers
+      include Locomotive::Steam::Middlewares::Concerns::Helpers
 
       def _call
         liquid_assigns['store'] = ShopInvader::Liquid::Drops::Store.new
