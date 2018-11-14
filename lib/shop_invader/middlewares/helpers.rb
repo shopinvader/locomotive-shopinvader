4efeb8da0e8db559a068c54e1606ec7800001273
Sébastien BEAU <sebastien.beau@akretion.com>
2018-11-14 23:58:01 +0100

    [IMP] set role in a specific cookie to be able to cache product depending of the role

--- lib/shop_invader/middlewares/helpers.rb
+++ lib/shop_invader/middlewares/helpers.rb
@@ -4,6 +4,7 @@ module Locomotive::Steam
     module Helpers
 
       alias_method :orig_render_response, :render_response
+      alias_method :orig_inject_cookies, :inject_cookies
 
       def render_response(content, code = 200, type = nil)
         status, headers, body = orig_render_response(content, code, type)
@@ -26,6 +27,20 @@ module Locomotive::Steam
         end
       end
 
+      def inject_cookies(headers)
+        role = customer && customer.role || default_role
+        # TODO make the max_age configurable maybe we should use the same age as the main cookie
+        request.env['steam.cookies']['role'] = {value: role, path: '/', max_age: 1.year}
+        orig_inject_cookies(headers)
+      end
+
+      def customer
+        @customer ||= request.env['authenticated_entry']
+      end
+
+      def default_role
+        @default_role ||= site.metafields['erp']['default_role']
+      end
     end
   end
 end
