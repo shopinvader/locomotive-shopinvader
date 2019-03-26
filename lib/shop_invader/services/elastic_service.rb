66bc0ab6adae27dbe3a1d1200b7f31e95572b3ad
Laurent-Corron <laurentcorron@gmail.com>
2019-03-26 09:11:31 +0100

    find_by_key working

--- lib/shop_invader/services/elastic_service.rb
+++ lib/shop_invader/services/elastic_service.rb
@@ -30,41 +30,25 @@ module ShopInvader
 
     def find_all_products_and_categories
       # pour tout les indices
+      Locomotive::Common::Logger.debug "[Elastic] find_all_products_and_categories"
       indices.map do |config|
         # crée un hash
         {}.tap do |records|
 
           site.locales.each do |locale|
-            # crée un objet index pour le rechercher
-            # lien de l'index: #{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}
             Locomotive::Common::Logger.debug "[Elastic] find_all_products_and_categories"
 
-            result = @client.search index: "#{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}", body: { query: { match_all: {} } }
+            # result = @client.search index: "#{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}", body: { query: { match_all: {} } }
 
             Locomotive::Common::Logger.debug "[Elastic find_all_products_and_categories] search all result: #{result}"
-            #
-            # index   = Algolia::Index.new("#{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}", @client)
-            # filters = ''
-            #
-            # if config['have_variant']
-            #     filter = 'main: true'
-            # end
-            #
-            # params = {
-            #    query: '',
-            #    attributesToRetrieve: 'name,objectID,url_key',
-            #    filters: filter,
-            # }
-            # index.browse(params) do |hit|
-            #   record = records[hit['objectID']] ||= {}
-            #   record[locale] = { name: hit['name'], url: find_route(config['name']).gsub('*', hit['url_key']) }
-            # end
           end
         end.values
       end.flatten
     end
 
     def find_all(name, conditions: nil, page: 1, per_page: 20)
+
+      Locomotive::Common::Logger.debug "[Elastic] find_all conditions:#{conditions}"
       # creating the body of the query
       #TODO handle page
       body = {
@@ -113,23 +97,26 @@ module ShopInvader
     def _find_by_key(index, name, key)
       Locomotive::Common::Logger.debug "[Elastic] found_by_key"
       Locomotive::Common::Logger.debug "[Elastic] given index: #{index}"
-
+      Locomotive::Common::Logger.debug "[Elastic] given key: #{key}"
 
       body = {
-        query: {
-          match: {
-            url_key: key
+        query:{
+          bool:{
+            filter: {
+              term: {
+                url_key: key
+              }
+            }
           }
         }
       }
+
+      body['size']=1
       Locomotive::Common::Logger.debug "[Elastic] body : #{body}"
       response = @client.search(
         index: index,
         body: body
       )
-      # if response["hits"]["total"]==0
-      #   response=''
-      # end
 
       Locomotive::Common::Logger.debug "[Elastic] response for #{name} : #{response}"
 
@@ -138,11 +125,12 @@ module ShopInvader
       # })
       response = _parse_response(response)
       resource = nil
+      
       # look for the main product/category AND its variants
       response['hits']['hits'].each do |hit|
         hit['index_name'] = name
         if resource.nil?
-          resource = hit
+          resource = hit['_source']
         else
           (resource['variants'] ||= []) << hit
         end
@@ -186,7 +174,7 @@ module ShopInvader
 
 
     def find_route(index_name)
-      @routes ||= JSON.parse(site.metafields.dig('algolia', 'routes')  || '[]')
+      @routes ||= JSON.parse(site.metafields.dig('elasticsearch', 'routes')  || '[]')
       (@routes.find { |(route, rule)| rule['index'] == index_name }).try(:first)
     end
 
