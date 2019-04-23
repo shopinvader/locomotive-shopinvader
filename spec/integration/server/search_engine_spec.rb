fef029283c6a2f1ece63cefc4cee51f65064b210
Sébastien BEAU <sebastien.beau@akretion.com>
2019-04-23 12:05:41 +0200

    Finish to add integration test for algolia

--- spec/integration/server/search_engine_spec.rb
+++ spec/integration/server/search_engine_spec.rb
@@ -23,17 +23,48 @@ describe 'Connected to the search engine' do
   end
 
   describe 'testing rendering a product page with variant' do
-    it 'get on "mid-century-armchair" return the product "Mid-Century Armchair (Red)"' do
+    it 'get on "mid-century-armchair" return the product "Mid-Century Armchair (Red) and with the variant"' do
         get 'mid-century-armchair'
       expect(last_response.status).to eq 200
-      expect(last_response.body).to eq "Mid-Century Armchair (Red)\n"
+      expect(last_response.body).to eq "Mid-Century Armchair (Red) Mid-Century Armchair (Yellow) Mid-Century Armchair (Blue)\n"
     end
 
-    it 'get on "fr/fauteuil-mid-century" return the french category "Fauteuil Mid-Century (Rouge)"' do
-        get 'fr/fauteuil-mid-century'
+    it 'get on "fr/fauteuil-mid-century" return the french category "Fauteuil Mid-Century (Rouge) with the variant"' do
+      get 'fr/fauteuil-mid-century'
       expect(last_response.status).to eq 200
-      expect(last_response.body).to eq "Fauteuil Mid-Century (Rouge)\n"
+      expect(last_response.body).to eq "Fauteuil Mid-Century (Rouge) Fauteuil Mid-Century (Jaune) Fauteuil Mid-Century (Bleu)\n"
     end
   end
 
+  describe 'Rendering the sitemap' do
+    it 'should include search engine content' do
+      get 'sitemap.xml'
+      expect(last_response.status).to eq 200
+      path = File.expand_path('../../data/expected_sitemap.xml', __FILE__)
+      data = File.read(path)
+      expect(last_response.body).to eq data
+    end
+  end
+
+  describe 'Rendering the drop' do
+
+    it 'store.categories should list all categories' do
+      get 'search-engine/store_categories'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq " Furniture Living room Bathroom\n"
+    end
+
+    it 'store.categories with scope level 0 should list one category' do
+      get 'search-engine/store_categories_with_level_0'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq " Furniture\n"
+    end
+
+    it 'store.products with paginate do the pagination' do
+      get 'search-engine/store_products_with_paginate'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq "\n TV Media Stand Mid-Century Armchair (Red) Mid-Century Armchair (Yellow)\ntotal page: 2\ntotal entries: 4\n\n"
+    end
+
+  end
 end
