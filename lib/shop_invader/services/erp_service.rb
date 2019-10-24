shopinvader/locomotive-shopinvader

commit fe895e2bc85e7a4e5164b70ef4a9c66762c079d7

[IMP] add locale mapping
allow to customize throught the metafields the locale mapping
you have to add the field "locale_mapping" in the metafields "_store"

lib/shop_invader/services/erp_service.rb
@@ -2,6 +2,7 @@ require 'digest'
 
 module ShopInvader
   class ErpService
+    include ShopInvader::Services::Concerns::LocaleMapping
     FORWARD_HEADER = %w(ACCEPT ACCEPT_ENCODING ACCEPT_LANGUAGE HOST REFERER ACCEPT USER_AGENT)
     attr_reader :client
     attr_reader :session
@@ -179,7 +180,7 @@ module ShopInvader
     def get_header_for_request(locale, request)
       headers = {
         api_key: @site.metafields['erp']['api_key'],
-        accept_language: ShopInvader::LOCALES[locale.to_s],
+        acept_language: map_locale(locale.to_s),
       }
       add_client_header(request, headers)
       add_header_info_from_session(headers)

lib/shop_invader.rb
@@ -6,6 +6,7 @@ require 'elasticsearch'
 require 'shop_invader/version'
 require 'shop_invader/errors'
 require 'shop_invader/services'
+require 'shop_invader/services/concerns/locale'
 require 'shop_invader/services/concerns/search_engine'
 require 'shop_invader/services/algolia_service'
 require 'shop_invader/services/elastic_service'

lib/shop_invader/services/algolia_service.rb
@@ -39,9 +39,8 @@ module ShopInvader
       indices.map do |config|
         {}.tap do |records|
           site.locales.each do |locale|
-            index   = Algolia::Index.new("#{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}", @client)
+            index   = Algolia::Index.new(build_index_name(config['index'], locale.to_s), @client)
             filters = ''
-
             if config['have_variant']
                 filter = 'main: true'
             end
@@ -140,7 +139,7 @@ module ShopInvader
     # as we want to keep the case sensitive
     # will be removed soon
     def build_index_name(index, locale)
-      "#{index}_#{ShopInvader::LOCALES[locale.to_s]}"
+      "#{index}_#{map_locale(locale.to_s)}"
     end
 
     def find_route(index_name)

lib/shop_invader/services/concerns/locale.rb
@@ -1,0 +1,16 @@
+module ShopInvader
+  module Services
+    module Concerns
+
+      module LocaleMapping
+
+        private
+
+        def map_locale(locale)
+          mapping = JSON.parse(@site.metafields.dig('_store', 'locale_mapping') || '{}')
+          mapping[@locale.to_s] || ShopInvader::LOCALES[locale.to_s]
+        end
+      end
+    end
+  end
+end

lib/shop_invader/services/concerns/search_engine.rb
@@ -3,6 +3,7 @@ module ShopInvader
     module Concerns
 
       module SearchEngine
+        include ShopInvader::Services::Concerns::LocaleMapping
 
         KEY_ATTRIBUTES = %w(url_key redirect_url_key).freeze
 
@@ -18,7 +19,7 @@ module ShopInvader
         end
 
         def build_index_name(index, locale)
-          "#{index}_#{ShopInvader::LOCALES[locale.to_s]}".downcase
+          "#{index}_#{map_locale(locale.to_s)}".downcase
         end
 
         def build_attr(name, value)

lib/shop_invader/services/elastic_service.rb
@@ -31,7 +31,7 @@ module ShopInvader
       indices.map do |config|
         {}.tap do |records|
           site.locales.each do |locale|
-            index   = "#{config['index']}_#{ShopInvader::LOCALES[locale.to_s]}".downcase
+            index   = build_index_name(config['index'], locale.to_s)
             body =  {
               query: { match_all: {} },
               sort: @default_sort[config['name']] || []

lib/shop_invader/services/search_engine_service.rb
@@ -3,7 +3,6 @@ module ShopInvader
     attr_reader :adapter
 
     def initialize(site, locale, elastic, algolia)
-      @locale    = ShopInvader::LOCALES[locale.to_s]
       if elastic.is_configured?
         @adapter = elastic
       elsif algolia.is_configured?

spec/services/algolia_service_spec.rb
@@ -18,6 +18,25 @@ RSpec.describe ShopInvader::AlgoliaService do
   let(:locale)    { 'fr' }
   let(:service)   { described_class.new(site, customer, locale) }
 
+  describe '#build_index_name for local fr' do
+
+    subject { service.send(:build_index_name, 'shopinvader_variant', 'fr') }
+
+    it 'returns the index with the lang fr_FR' do
+      expect(subject).to eq('shopinvader_variant_fr_FR')
+    end
+
+    context "with specific lang mapping for Belgium" do
+
+      let(:metafields) { {'_store' => {'locale_mapping' => '{"fr": "fr_BE"}'}} }
+
+      it 'return the index with the lang fr_be' do
+        expect(subject).to eq('shopinvader_variant_fr_BE')
+      end
+
+    end
+  end
+
   describe 'Building params from condition' do
     subject { service.send(:build_params, conditions) }
 

commit c4fea698380921cc0bc7b40bc55681adf8305730

[FIX] selected lang is no more properly forwarded to Odoo fix typo into header introduced in fe895e2bc85e7a4e5164b70ef4a9c66762c079d7

lib/shop_invader/services/erp_service.rb
@@ -182,7 +182,7 @@ module ShopInvader
     def get_header_for_request(locale, request)
       headers = {
         api_key: @site.metafields['erp']['api_key'],
-        acept_language: map_locale(locale.to_s),
+        accept_language: map_locale(locale.to_s),
       }
       add_client_header(request, headers)
       add_header_info_from_session(headers)